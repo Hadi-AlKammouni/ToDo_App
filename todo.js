92e73fec468de9762269b5823550763befde5945
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-02 17:23:01 +0300

    js// Declaring the needed variables

---
+++ todo.js
@@ -0,0 +1,10 @@
+// Declaring the required variables
+let title = $("#title");
+let description = $("#description");
+let add_todo = $("#add-todo");
+let importance = $("#importance");
+let item_lists = $("#item-lists");
+
+let id_counter = 1;
+let added_values=[];
+let task_number = 1;
