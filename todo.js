1c832b86124187d2e8bcb621ac7bef943475a777
Hadi-AlKammouni <hadi-kam99@hotmail.com>
2022-06-02 17:28:01 +0300

    js// Filling the data in the table upon opening the browser

--- todo.js
+++ todo.js
@@ -9,6 +9,30 @@ let id_counter = 1;
 let added_values=[];
 let task_number = 1;
 
+// Upon opening the browser, if there is data saved in the local storage, 
+// they will be filled directly in the table
+
+for(let i=0; i<localStorage.length; i++){
+    // Getting the data from the local storage
+    let x= localStorage.getItem(`task ${task_number}`);
+    let y = JSON.parse(x);
+    task_number++;
+        
+    // Putting the values of each task in the array to iterate over them for each row
+    values=[id_counter,y.Title,y.Description,y.Importance,y.Done,y.CreatedAt];
+
+    // Creating new row in the table + Filling the data in the columns
+    let new_row = document.createElement("tr");
+    item_lists.append(new_row);
+    for (let i=0; i<6; i++){
+        new_task = document.createElement("td");
+        new_task.append(document.createTextNode(values[i]));
+        new_row.append(new_task);
+    }
+
+    id_counter++; // To auto increment the id
+}
+
 // Upon clicking the add to list button:
 add_todo.click(function(){
 
